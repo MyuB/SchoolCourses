4530742d56eeb0063d0b24745b3431c19fd2a657
MyuB <mjycoco0@naver.com>
2022-07-25 18:13:29 +0900

    sample added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const Container = styled.div`
 export default function App() {
   return (
     <Container>
-      <CourseCard />
+      <CourseCard isFree={false} />
     </Container>
   );
 }
--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -7,8 +7,10 @@ const Container = styled.div`
   width: 296px;
   height: 407px;
   box-sizing: border-box;
-  padding: 28px 24px 0;
+  padding: 28px 24px 20px;
   position: relative;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Tags = styled.p`
@@ -42,20 +44,78 @@ const DividerLine = styled.div`
   bottom: 64px;
 `;
 
+const CostWrapper = styled.div`
+  margin-top: auto;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const CurrentCost = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  color: #151618;
+`;
+
+const OriginCost = styled.p`
+  font-size: 14px;
+  line-height: 16px;
+  text-decoration-line: line-through;
+  color: #a8a9ab;
+  margin-left: 8px;
+`;
+
+const DiscoutPercentile = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  color: #f94669;
+  margin-left: auto;
+`;
+
+const CostFree = styled.div`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  color: #34ab53;
+  margin-top: auto;
+`;
+
 CourseCard.defaultProps = {
   tags: ["태그1", "태그2"],
   title: "샘플 타이틀",
   description: "프로그래밍이 처음이신가요? 파이썬으로 쉽고 재밌게 시작해봐요.",
+  isFree: true,
+  currentCost: 42000,
+  originalCost: 57000,
+  discountPercentile: 35,
 };
 
 // styled component를 과목카드를 만드세요.
-export default function CourseCard({ tags, title, description }) {
+export default function CourseCard({
+  tags,
+  title,
+  description,
+  isFree,
+  currentCost,
+  originalCost,
+  discountPercentile,
+}) {
   return (
     <Container>
       <Tags>{tags.join("﹒")}</Tags>
       <Title>{title}</Title>
       <Description>{description}</Description>
       <DividerLine />
+      {isFree ? (
+        <CostFree>무료</CostFree>
+      ) : (
+        <CostWrapper>
+          <CurrentCost>{currentCost.toLocaleString()}원</CurrentCost>
+          <OriginCost>{originalCost.toLocaleString()}원</OriginCost>
+          <DiscoutPercentile>{discountPercentile}%</DiscoutPercentile>
+        </CostWrapper>
+      )}
     </Container>
   );
 }
